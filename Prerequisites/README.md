b5230c5f5bd201b79f68237ec9b806d40a700000
Franck Cornu <franck.cornu@ubisoft.com>
2024-08-02 16:00:48 -0400

    - Added prerequisites

---
+++ Prerequisites/README.md
@@ -0,0 +1,9 @@
+# Building a SharePoint Web Part - Using Microsoft Graph API to get items
+
+The following prerequisites needs to be deployed on a SharePoint site to be able to follow the section "Using Microsoft Graph API to get items".
+
+1. Install the [`PnP.PowerShell`](https://pnp.github.io/powershell/articles/installation.html) PowerShell module locally. It requires PowerShell 7.2 or later.
+
+> The following script has been tested with `PnP.PowerShell` version **2.5.0**.
+
+2. Run the `deploy.ps1 -SiteUrl <your_sharepoint_site_url>` script to deploy the solution on your SharePoint site.
---
+++ Prerequisites/deploy.ps1
@@ -0,0 +1,55 @@
+[CmdletBinding()]
+Param (
+    [Parameter(Mandatory=$true)]
+    [string]$SiteUrl
+)
+
+#$SiteUrl = "https://sonbaedev.sharepoint.com/sites/packt2"
+
+Connect-PnPOnline -Url $SiteUrl -Interactive
+
+# Apply site template
+Invoke-PnPSiteTemplate -Path "./template.xml"
+
+$Pictures = @{
+    "UltraBoost Running Shoes" = "ultraboost.jpg"
+    "Tech Fleece Hoodie" = "techfleece.jpg"
+    "Futura Tee Shirt" = "futura.jpg"
+}
+
+$ProductItems = @(
+    @{
+        Title = "UltraBoost Running Shoes"
+        packtProductModelName = "UltraBoost Running Shoes"
+        packtProductRetailPrice = 180
+        packtProductReference = "UB-001"
+        packtProductStockLevel = 25
+        packtProductColor = "Red"
+        packtProductSize = "S"
+    },
+    @{
+        Title = "Tech Fleece Hoodie"
+        packtProductModelName = "Tech Fleece Hoodie"
+        packtProductRetailPrice = 100
+        packtProductReference = "TF-002"
+        packtProductStockLevel = 40
+        packtProductColor = "Black"
+        packtProductSize = "M"
+    }
+    @{
+        Title = "Futura Tee Shirt"
+        packtProductModelName = "Futura Tee Shirt"
+        packtProductRetailPrice = 75
+        packtProductReference = "FT-002"
+        packtProductStockLevel = 23
+        packtProductColor = "Blue"
+        packtProductSize = "L"
+    }
+)
+
+# Add products list items
+$ProductItems | ForEach-Object {
+    $item = Add-PnPListItem -List "Products" -Values $_
+    Set-PnPImageListItemColumn -List "Products" -Identity $item -Field "packtProductItemPicture" -Path "./assets/$($Pictures[$_.Title])"
+}
+
