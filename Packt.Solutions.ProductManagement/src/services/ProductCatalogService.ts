788bb8ba09be0c60051afa6613a777027d67b425
Franck Cornu <franck.cornu@outlook.com>
2025-01-02 22:28:35 -0500

    - Updated samples

--- Packt.Solutions.ProductManagement/src/services/ProductCatalogService.ts
+++ Packt.Solutions.ProductManagement/src/services/ProductCatalogService.ts
@@ -4,29 +4,51 @@ import {
   ProductSizes,
 } from "../models/IProductCatalogItem";
 import { IProductCatalogService } from "./IProductCatalogService";
-import { SPHttpClient } from "@microsoft/sp-http";
-//import { MSGraphClientV3 } from "@microsoft/sp-http";
+import { MSGraphClientV3 } from "@microsoft/sp-http";
+// import { SPHttpClient } from "@microsoft/sp-http";
+// import { HttpClient } from "@microsoft/sp-http";
+// import { AadHttpClient } from "@microsoft/sp-http";
 
 export class ProductCatalogService implements IProductCatalogService {
-  //private _msGraphClient: MSGraphClientV3;
-  private _spHttpClient: SPHttpClient;
-  private _webUrl: string;
 
-  /*constructor(msGraphClient: MSGraphClientV3) {
+  /* Uncomment regions according to your scenario */
+
+  private _msGraphClient: MSGraphClientV3;
+  // private _spHttpClient: SPHttpClient;
+  // private _webUrl: string;
+  // private _httpClient: HttpClient;
+  // private _aadHttpClient: AadHttpClient;
+
+  //#region MSGraphClientV3
+  constructor(msGraphClient: MSGraphClientV3) {
     this._msGraphClient = msGraphClient;
-  }*/
-
-  constructor(spHttpClient: SPHttpClient, webUrl: string) {
-    this._spHttpClient = spHttpClient;
-    this._webUrl = webUrl;
   }
+  //#endregion
 
-  // Example using SharePoint REST API and SPHttpClient
+  //#region SpHttpClient
+  // constructor(spHttpClient: SPHttpClient, webUrl: string) {
+  //   this._spHttpClient = spHttpClient;
+  //   this._webUrl = webUrl;
+  // }
+  //#endregion
+
+  //#region HttpClient
+  // constructor(httpClient: HttpClient) {
+  //   this._httpClient = httpClient;
+  // }
+  //#endregion
+
+  //#region AadHttpClient
+  // constructor(aadHttpClient: AadHttpClient) {
+  //   this._aadHttpClient = aadHttpClient;
+  // }
+  //#endregion
+
+  //#region Example using Microsoft Graph API and MSGraphClientV3
   public async getProducts(
     siteId: string,
     listName: string
   ): Promise<IProductCatalogItem[]> {
-
     // SharePoint columns for a product
     const fields = [
       "packtProductColor",
@@ -38,64 +60,11 @@ export class ProductCatalogService implements IProductCatalogService {
       "packtProductStockLastOrderDate",
       "packtProductStockLevel"
     ];
-
-    const response = await this._spHttpClient.get(
-      `${this._webUrl}/_api/web/lists/GetByTitle('${listName}')/items?$select=${fields}`,
-      SPHttpClient.configurations.v1);
-    
-    if (response.ok) {
-      const responseJson = await response.json();
-
-      const items: IProductCatalogItem[] = responseJson.value.map((item: any) => {
-        return {
-          modelName: item.packtProductModelName,
-          lastOrderDate: item.packtProductStockLastOrderDate
-            ? new Date(item.packtProductStockLastOrderDate)
-            : null,
-          productReference: item.packtProductReference,
-          stockLevel: item.packtProductStockLevel,
-          size: item.packtProductSize as ProductSizes,
-          retailPrice: item.packtProductRetailPrice,
-          itemColour: item.packtProductColor,
-          itemPicture: item.packtProductItemPicture
-            ? JSON.parse(item.packtProductItemPicture).serverRelativeUrl
-            : null,
-        } as IProductCatalogItem;
-      });
-      
-      return items;
-    } else {
-      Log.error("ProductCatalogService", new Error(response.statusText));
-      return [];
-    }
-  }
-  
-  // Example using Microsoft Graph API and MSGraphClientV3
-  /*
-  public async getProducts(
-    siteId: string,
-    listName: string
-  ): Promise<IProductCatalogItem[]> {
-
-    // SharePoint columns for a product
-    const fields = [
-      "packtProductColor",
-      "packtProductModelName",
-      "packtProductItemPicture",
-      "packtProductReference",
-      "packtProductRetailPrice",
-      "packtProductSize",
-      "packtProductStockLastOrderDate",
-      "packtProductStockLevel"
-    ];
-
     try {
-      
       const response = await this._msGraphClient
             .api(`sites/${siteId}/lists/${listName}/items`)
             .expand(`fields($select=${fields})`)
             .get();
-
           // eslint-disable-next-line @typescript-eslint/no-explicit-any
           const items: IProductCatalogItem[] = response.value.map((item: any) => {
             return {
@@ -113,11 +82,132 @@ export class ProductCatalogService implements IProductCatalogService {
                 : null,
             } as IProductCatalogItem;
           });
-
           return items;          
     } catch (error) {
       Log.error("ProductCatalogService", error);
       return [];
     }
-  }*/
+  }
+  //#endregion
+
+  //#region Example using SharePoint REST API and SPHttpClient
+  // public async getProducts(
+  //   siteId: string,
+  //   listName: string
+  // ): Promise<IProductCatalogItem[]> {
+
+  //   // SharePoint columns for a product
+  //   const fields = [
+  //     "packtProductColor",
+  //     "packtProductModelName",
+  //     "packtProductItemPicture",
+  //     "packtProductReference",
+  //     "packtProductRetailPrice",
+  //     "packtProductSize",
+  //     "packtProductStockLastOrderDate",
+  //     "packtProductStockLevel"
+  //   ];
+
+  //   const response = await this._spHttpClient.get(
+  //     `${this._webUrl}/_api/web/lists/GetByTitle('${listName}')/items?$select=${fields}`,
+  //     SPHttpClient.configurations.v1, );
+    
+  //   if (response.ok) {
+  //     const responseJson = await response.json();
+
+  //     const items: IProductCatalogItem[] = responseJson.value.map((item: any) => {
+  //       return {
+  //         modelName: item.packtProductModelName,
+  //         lastOrderDate: item.packtProductStockLastOrderDate
+  //           ? new Date(item.packtProductStockLastOrderDate)
+  //           : null,
+  //         productReference: item.packtProductReference,
+  //         stockLevel: item.packtProductStockLevel,
+  //         size: item.packtProductSize as ProductSizes,
+  //         retailPrice: item.packtProductRetailPrice,
+  //         itemColour: item.packtProductColor,
+  //         itemPicture: item.packtProductItemPicture
+  //           ? JSON.parse(item.packtProductItemPicture).serverRelativeUrl
+  //           : null,
+  //       } as IProductCatalogItem;
+  //     });
+  //     return items;
+  //   } else {
+  //     Log.error("ProductCatalogService", new Error(response.statusText));
+  //     return [];
+  //   }
+  // }
+  //#endregion
+  
+  //#region Example using Anonymous API and HttpClient
+  // public async getProducts(
+  //   siteId: string,
+  //   listName: string
+  // ): Promise<IProductCatalogItem[]> {
+
+  //     const response = await this._httpClient.get("https://demospfxfunction.azurewebsites.net/api/GetProducts", HttpClient.configurations.v1);
+      
+  //     if (response.ok) {
+
+  //         const responseJson = await response.json();
+      
+  //         // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  //         const items: IProductCatalogItem[] = responseJson.map((item: any) => {
+  //             return {
+  //               modelName: item.packtProductModelName,
+  //               lastOrderDate: item.packtProductStockLastOrderDate
+  //                 ? new Date(item.packtProductStockLastOrderDate)
+  //                 : null,
+  //               productReference: item.packtProductReference,
+  //               stockLevel: item.packtProductStockLevel,
+  //               size: item.packtProductSize as ProductSizes,
+  //               retailPrice: item.packtProductRetailPrice,
+  //               itemColour: item.packtProductColor,
+  //               itemPicture: item.packtProductItemPicture
+  //             } as IProductCatalogItem;
+  //           });
+
+  //         return items;          
+  //   } else {
+  //     Log.error("ProductCatalogService", new Error(response.statusText));
+  //     return [];
+  //   }
+  // }
+  //#endregion
+
+  //#region Example using custom Entra ID protected API and AadHttpClient
+  // public async getProducts(
+  //   siteId: string,
+  //   listName: string
+  // ): Promise<IProductCatalogItem[]> {
+
+  //     const response = await this._aadHttpClient.get("https://demospfxfunction.azurewebsites.net/api/GetProducts", AadHttpClient.configurations.v1);
+      
+  //     if (response.ok) {
+
+  //         const responseJson = await response.json();
+      
+  //         // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  //         const items: IProductCatalogItem[] = responseJson.map((item: any) => {
+  //           return {
+  //             modelName: item.packtProductModelName,
+  //             lastOrderDate: item.packtProductStockLastOrderDate
+  //               ? new Date(item.packtProductStockLastOrderDate)
+  //               : null,
+  //             productReference: item.packtProductReference,
+  //             stockLevel: item.packtProductStockLevel,
+  //             size: item.packtProductSize as ProductSizes,
+  //             retailPrice: item.packtProductRetailPrice,
+  //             itemColour: item.packtProductColor,
+  //             itemPicture: item.packtProductItemPicture
+  //           } as IProductCatalogItem;
+  //         });
+
+  //         return items;          
+  //   } else {
+  //     Log.error("ProductCatalogService", new Error(response.statusText));
+  //     return [];
+  //   }
+  // }
+  //#endregion
 }
--- Packt.Solutions.ProductManagement/src/webparts/packProductCatalog/PackProductCatalogWebPart.ts
+++ Packt.Solutions.ProductManagement/src/webparts/packProductCatalog/PackProductCatalogWebPart.ts
@@ -33,12 +33,36 @@ export default class PackProductCatalogWebPart extends BaseClientSideWebPart<IPa
 
   protected async onInit(): Promise<void> {
 
-    // Usage with MS Graph API
-    /*const msGraphClient = await this.context.msGraphClientFactory.getClient("3");
-    this._productCatalogService = new ProductCatalogService(msGraphClient);*/
+    //#region  Usage with MS Graph API
+    const msGraphClient = await this.context.msGraphClientFactory.getClient("3");
+    this._productCatalogService = new ProductCatalogService(msGraphClient);
+    //#endregion
 
-    // Usage with SharePoint REST API
-    this._productCatalogService = new ProductCatalogService(this.context.spHttpClient, this.context.pageContext.web.absoluteUrl);
+    //#region  Usage with SharePoint REST API
+    //this._productCatalogService = new ProductCatalogService(this.context.spHttpClient, this.context.pageContext.web.absoluteUrl);
+    //#endregion
+
+    //#region  Usage with regular HTTP client
+    //this._productCatalogService = new ProductCatalogService(this.context.httpClient);
+    //#endregion
+
+    //#region  Usage with AadHttpClient
+    // In this example, the client ID of the Entra ID application is "4df79ed7-c568-499a-b1c2-6abdcab5d4bf"
+    // const aadHttpClient = await this.context.aadHttpClientFactory.getClient("4df79ed7-c568-499a-b1c2-6abdcab5d4bf");
+    // this._productCatalogService = new ProductCatalogService(aadHttpClient);
+    //#endregion
+
+    //#region  Usage of AadTokenProvider
+    // const aadTokenProvider = await this.context.aadTokenProviderFactory.getTokenProvider();
+
+    // aadTokenProvider.getToken("https://graph.microsoft.com").then((token) => {
+    //   console.log(token);
+    // });
+    
+    // aadTokenProvider.getToken("4df79ed7-c568-499a-b1c2-6abdcab5d4bf").then((token) => {
+    //   console.log(token);
+    // });
+    //#endregion
 
     return super.onInit();
   }
@@ -50,7 +74,6 @@ export default class PackProductCatalogWebPart extends BaseClientSideWebPart<IPa
   protected get dataVersion(): Version {
     return Version.parse('1.0');
   }
-
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
     return {
