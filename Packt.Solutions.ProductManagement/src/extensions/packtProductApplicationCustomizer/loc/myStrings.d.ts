3965eead11ad2b0240d7260ae6811040286dd6fb
Anoop T <anoop.tatti@gmail.com>
2025-01-06 22:46:53 +0000

    feat: added application customizer

---
+++ Packt.Solutions.ProductManagement/src/extensions/packtProductApplicationCustomizer/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IPacktProductApplicationCustomizerApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'PacktProductApplicationCustomizerApplicationCustomizerStrings' {
+  const strings: IPacktProductApplicationCustomizerApplicationCustomizerStrings;
+  export = strings;
+}
